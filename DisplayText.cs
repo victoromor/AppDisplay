c636c26cf56c1f9750a33940a177c801e0e0de23
SirVic <victorakwara83@gmail.com>
2023-01-16 01:50:35 -0800

    commit

---
+++ DisplayText.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace AppDisplay
+{
+    public class DisplayText
+    {
+        public void Display(string text) // Displays text to console
+        {
+            Console.WriteLine(text);
+        }
+    }
+
+}
